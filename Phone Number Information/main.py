9e206d86298e0a473a63437146b1c85fb00ae18a
4nupam <101491219+4nupam@users.noreply.github.com>
2024-04-05 11:21:05 +0530

    Update main.py

--- Phone Number Information/main.py
+++ Phone Number Information/main.py
@@ -1,24 +1,33 @@
 import phonenumbers
-from phonenumbers import geocoder,carrier
+from phonenumbers import geocoder, carrier
 
-def track_country(phone_number):
+def track_location(phone_number):
     try:
-       prased_number = phonenumbers.parse(phone_number,"CH")
-       print(geocoder.description_for_number(prased_number,"en")) 
+        parsed_number = phonenumbers.parse(phone_number, "CH")
+        print(geocoder.description_for_number(parsed_number, "en"))
     except phonenumbers.phonenumberutil.NumberParseException as e:
-       print("Error ",e)
+        print("Error:", e)
 
 def track_company(phone_number):
     try:
-      prased_number = phonenumbers.parse(phone_number,"CH")
-      print(carrier.name_for_number(prased_number,"en"))
-
+       carrier_number = phonenumbers.parse(phone_number, "RO")
+       print(carrier.name_for_number(carrier_number, "en"))
     except phonenumbers.phonenumberutil.NumberParseException as e:
-       print("Error",e)
+        print("Error:", e)
+
+def track_state(phone_number):
+    try:
+        parsed_number = phonenumbers.parse(phone_number, "CH")
+        city = geocoder.description_for_valid_number(parsed_number, "en")
+        print(city.split(', ')[-1])
+         # Extracting state
+    except phonenumbers.phonenumberutil.NumberParseException as e:
+        print("Error:", e)
 
 def main():
-   phone_number = input("Enter the number ")
-   track_country(phone_number)
-   track_company(phone_number)
+    phone_number = input("Enter the phone number with country code ")
+    track_location(phone_number)
+    track_company(phone_number)
+    track_state(phone_number)
 
 main()
